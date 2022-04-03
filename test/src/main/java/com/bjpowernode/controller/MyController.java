05d18690bb39fb240725b397ab039de59c77a8e2
wzx1914 <1140087313@qq.com>
2022-04-03 21:31:40 +0800

    1

---
+++ test/src/main/java/com/bjpowernode/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wzx
+ * 2022/4/3 18:05
+ */
+@Controller
+public class MyController {
+    @RequestMapping("/doquery")
+    public String doQuery(Model model){
+        model.addAttribute("name","name");
+        return "test";
+    }
+}
